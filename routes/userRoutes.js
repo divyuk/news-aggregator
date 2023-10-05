da6e566a0027378557da27d6bd5d8492e65a685c
divyuk <div14kes@gmail.com>
2023-10-05 18:00:03 +0530

    basic Skelton for preference

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const authController = require("../controllers/authController");
+const userController = require("../controllers/userController");
 
 const router = express.Router();
 
 router.post("/register", authController.register);
 router.get("/login", authController.login);
+
+router.get("/preferences", userController.preferences);
 
 module.exports = router;
 
