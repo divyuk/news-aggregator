2f86d717dcf2c3c124a0ffad1905f864fefe5a9a
divyuk <div14kes@gmail.com>
2023-10-06 11:37:54 +0530

    PUT Route Implemented

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,32 @@
 const User = require("../models/userModel");
+const AppError = require("../utils/appError");
 const catchAsyn = require("../utils/catchAsyn");
+
+const filterObj = (obj, ...allowedFields) => {
+  newObj = {};
+  Object.keys(obj).forEach((key) => {
+    if (allowedFields.includes(key)) newObj[key] = obj[key];
+  });
+  return newObj;
+};
 
 exports.preferences = catchAsyn(async (req, res, next) => {
   const { category, sources, country } = await User.findById(req.user);
   res
     .status(200)
     .json({ status: "success", data: { category, sources, country } });
+});
+
+exports.updatePreferences = catchAsyn(async (req, res, next) => {
+  if (req.body.password || req.body.passwordConfirm)
+    return next(AppError("This route is not for updating password", 400));
+
+  //! Filter out unawanted fields and only include whats needed
+  const filteredObj = filterObj(req.body, "category", "sources", "country");
+  const updatedPrefernces = await User.findByIdAndUpdate(
+    req.user.id,
+    filteredObj,
+    { new: true, runValidators: true }
+  );
+  res.status(200).json({ status: "success", data: updatedPrefernces });
 });
