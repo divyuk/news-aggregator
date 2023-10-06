451716ac8cad70065c52da057a792e72b5869b1f
divyuk <div14kes@gmail.com>
2023-10-06 10:55:02 +0530

    GET request for preferences

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,9 @@
-exports.preferences = (req, res) => {
-  console.log("I am preference", req.user);
-};
+const User = require("../models/userModel");
+const catchAsyn = require("../utils/catchAsyn");
+
+exports.preferences = catchAsyn(async (req, res, next) => {
+  const { category, sources, country } = await User.findById(req.user);
+  res
+    .status(200)
+    .json({ status: "success", data: { category, sources, country } });
+});
