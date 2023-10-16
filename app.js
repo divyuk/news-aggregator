e72582909bbe6f9b205ed21b99791dc584219f24
divyuk <div14kes@gmail.com>
2023-10-16 16:40:19 +0530

    Cors Configuration

--- app.js
+++ app.js
@@ -3,11 +3,21 @@ const dotenv = require("dotenv");
 const userRouter = require("./routes/userRoutes");
 const newsRouter = require("./routes/newsRoutes");
 const { default: mongoose } = require("mongoose");
+const cors = require("cors");
 const globalErrorHandler = require("./controllers/errorController");
 
 const app = express();
 // Middleware for parsing json.
 app.use(express.json());
+const corsOptions = {
+  origin: "http://localhost:5173",
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"], // specified here
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+express.urlencoded({ extended: false });
+
 dotenv.config({ path: "./config.env" });
 
 //? Database Connection
