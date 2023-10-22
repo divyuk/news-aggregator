475ad512e4281b6354cbb190e9412545a95c983e
divyuk <div14kes@gmail.com>
2023-10-22 15:51:14 +0530

    Validate if the passed name and username is correct or not

--- helpers/validate.js
+++ helpers/validate.js
@@ -1,5 +1,7 @@
 const { check } = require("express-validator");
 
+const usernameRegex = /^[A-Za-z0-9._]*$/;
+
 const validateRegister = [
   check("email")
     .isEmail()
@@ -8,6 +10,20 @@ const validateRegister = [
   check("password")
     .isLength({ min: 8 })
     .withMessage("Password must be at least 8 characters long"),
+  check("name")
+    .isString()
+    .not()
+    .isEmpty()
+    .withMessage("Name must not be empty and should be a string"),
+  check("username")
+    .isString()
+    .not()
+    .isEmpty()
+    .withMessage("Username must not be empty and should be a string")
+    .matches(usernameRegex)
+    .withMessage(
+      "Username can only contain letters, numbers, underscores, and periods"
+    ),
 ];
 
 const validateLogin = [
